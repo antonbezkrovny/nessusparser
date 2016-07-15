079c07236f9061a1c4eacdb24773076e23e8baec
anton.bezkrovny <anton.bezkrovnyy@gmail.com>
2016-07-15 12:59:29 +0300

    Connect and download

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#from nessrest import ness6rest
-#import io
-#scanner = ness6rest.Scanner(url="https://10.5.54.10:8834", login="a-bezkrovny", password=userpassword, insecure=True)
-#scanner.action(action="scans", method="get")
-#scanner.scan_id = 1026
-#with io.open('D:\\github\\nessusparser\\tmpscan.tmp', 'wt') as fp:
-#    fp.write(scanner.download_scan(export_format="nessus"))
-
 from nessmanager import ScannerConnector
 
 nessus_url="https://10.5.54.10:8834"
